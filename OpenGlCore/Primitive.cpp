4575b647b238814d9b22039393172b4b956658ff
sayantan sadhukhan <sayantansk13@gmail.com>
2024-04-19 17:01:31 +0530

    Implemented For Sphere to draw

--- OpenGlCore/Primitive.cpp
+++ OpenGlCore/Primitive.cpp
@@ -5,7 +5,7 @@ Primitive::Primitive(PrimitiveType a_type)
 	switch (a_type)
 	{
 	case sphere:
-		break;
+		GenerateSphere();
 	case pyramid:
 		GeneratePyramid();
 		break;
@@ -18,25 +18,78 @@ Primitive::Primitive(PrimitiveType a_type)
 	}
 }
 
+void Primitive::GenerateSphere()
+{
+
+	float radius = 0.5f;
+	int numSegments = 50;
+	int numStacks = 50;
+	GLfloat Vertices[7803];
+	GLuint indices[15000];
+	for (int stack = 0; stack <= numStacks; ++stack) {
+		for (int segment = 0; segment <= numSegments; ++segment) {
+			float theta = stack * M_PI / numStacks;
+			float phi = segment * 2 * M_PI / numSegments;
+
+			GLfloat x = radius * sin(theta) * cos(phi);
+			GLfloat y = radius * sin(theta) * sin(phi);
+			GLfloat z = radius * cos(theta);
+
+			int iter = 3 * (stack * (numSegments + 1) + segment);
+			Vertices[iter] = x;
+			Vertices[iter + 1] = y;
+			Vertices[iter + 2] = z;
+			/*vertices[index + 1] = y;
+			vertices[index + 2] = z;*/
+		}
+	}
+	int index = 0;
+	for (int stack = 0; stack < numStacks; ++stack) {
+		for (int segment = 0; segment < numSegments; ++segment) {
+			indices[index++] = stack * (numSegments + 1) + segment;
+			indices[index++] = (stack + 1) * (numSegments + 1) + segment;
+			indices[index++] = stack * (numSegments + 1) + segment + 1;
+
+			indices[index++] = stack * (numSegments + 1) + segment + 1;
+			indices[index++] = (stack + 1) * (numSegments + 1) + segment;
+			indices[index++] = (stack + 1) * (numSegments + 1) + segment + 1;
+		}
+	}
+	for (size_t i = 0; i < sizeof(Vertices) / sizeof(GLfloat); i++)
+	{
+		vertices.push_back(Vertices[i]);
+	}
+	for (size_t i = 0; i < sizeof(indices) / sizeof(GLuint); i++)
+	{
+		Indices.push_back(indices[i]);
+	}
+
+	//for (auto i : Vertices) {
+	//	vertices->push_back(i);
+	//}
+	//for (auto i : indices) {
+	//	Indices->push_back(i);
+	//}
+
+
+}
+
+
 void Primitive::GeneratePyramid()
 {
-
-	vertices = new std::vector<GLfloat>;
-	Indices = new std::vector<GLuint>();
-
 	for (auto i : Pyramid) {
-		vertices->push_back(i);
+		vertices.push_back(i);
 	}
 	for (auto i : PyramiDIndices) {
-		Indices->push_back(i);
+		Indices.push_back(i);
 	}
 }
 
-int Primitive::VerticesStride()
+int Primitive::VerticesStride() const
 {
-	return vertices->size() * 4;
+	return vertices.size() * 4;
 }
 
-int Primitive::TriangleStride() {
-	return Indices->size() * 4;
+int Primitive::TriangleStride() const {
+	return Indices.size() * 4;
 }
